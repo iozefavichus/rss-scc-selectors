2dc70c5c09f40c26c5f7207d5450a46b1175f39c
iozefavichus <iozefavichus@gmail.com>
2023-06-14 10:28:41 +0300

    fix: configure webpack.config

--- rss-css-selectors/webpack.config.js
+++ rss-css-selectors/webpack.config.js
@@ -1,9 +1,51 @@
-var path = require("path");
+/* eslint-disable no-undef */
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const copyPlugin = require('copy-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-module.exports = {
-  entry: "./assets/js/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: "ts-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new EslingPlugin({ extensions: 'ts' }),
+        new copyPlugin({
+            patterns: [
+                { from: "src/images", to: "../dist/images" },
+            ]
+        }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
 };
