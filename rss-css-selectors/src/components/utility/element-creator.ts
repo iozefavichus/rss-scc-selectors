5d2b0503783f7471b9e887d84624bcc6fda2ebe4
iozefavichus <iozefavichus@gmail.com>
2023-06-16 09:57:59 +0300

    feat: add footer css

--- rss-css-selectors/src/components/utility/element-creator.ts
+++ rss-css-selectors/src/components/utility/element-creator.ts
@@ -1,25 +1,37 @@
-import { ElemenetsParams } from 'types/index';
+import { ElementsParams } from '../../../src/types/index';
 
 
 export default class ElementCreator {
-    constructor(param) {
+    constructor(param: ElementsParams) {
         this.element = null;
         this.createElement(param);
     }
 
-    createElement(param){
+    getElement() {
+        return this.element;
+    }
+    addInnerElement(element) {
+        if (element instanceof ElementCreator) {
+            this.element.append(element.getElement());
+        } else {
+            this.element.append(element);
+        }
+    }
+
+    createElement(param: ElementsParams){
         this.element = document.createElement(param.tag);
         this.setCssClasses(param.classNames);
         this.setTextcontent(param.textContent);
         this.setCallback(param.callback);
     }
+
     setCssClasses(cssClasses: Array<string>){
         cssClasses.forEach((className) => this.element.classList.add(className));
     }
     setTextcontent(text: string){
         this.createElement.textContent = text;
     }
-    setCallback(callback){
+    setCallback(callback: () => void){
         this.element.addEventListener('click', (event) => callback(event))
     }
 }
--- rss-css-selectors/src/types/index.ts
+++ rss-css-selectors/src/types/index.ts
@@ -2,5 +2,5 @@ export type ElementsParams = {
     tag: string,
     classNames: Array<string>,
     textContent: string,
-    callBack(): void,
+    callback(): void,
 }
