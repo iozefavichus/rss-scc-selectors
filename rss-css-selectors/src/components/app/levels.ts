101adc7c34ea14bcdf7a46255d23f6289b113c91
iozefavichus <iozefavichus@gmail.com>
2023-07-02 14:17:08 +0200

    feat: add levels info

--- rss-css-selectors/src/components/app/levels.ts
+++ rss-css-selectors/src/components/app/levels.ts
@@ -67,5 +67,94 @@ export const levels = [
     </plate>
     <apple/>
     `
+  },
+  {
+    helpTitle: "Combine the Descendant & ID Selectors",
+    selectorName : "",
+    doThis : "Select the pickle on the fancy plate",
+    selector : "#fancy pickle",
+    syntax: "#id&nbsp;&nbsp;A",
+    help : 'You can combine any selector with the descendent selector.',
+    examples : [
+      '<strong>#cool&nbsp;span</strong> selects all <tag>span</tag> elements that are inside of elements with <strong>id="cool"</strong>'
+    ],
+    boardMarkup : `
+    <bento>
+    <orange/>
+    </bento>
+    <plate id="fancy">
+      <pickle/>
+    </plate>
+    <plate>
+      <pickle/>
+    </plate>
+    `
+  },
+  {
+    helpTitle: "Select elements by their class",
+    selectorName: "Class Selector",
+    doThis : "Select the small apples",
+    selector : ".small",
+    syntax: ".classname",
+    help : 'The class selector selects all elements with that class attribute. Elements can only have one ID, but many classes.',
+    examples : [
+    '<strong>.neato</strong> selects all elements with <strong>class="neato"</strong>'
+    ],
+    boardMarkup : `
+    <apple/>
+    <apple class="small"/>
+    <plate>
+      <apple class="small"/>
+    </plate>
+    <plate/>
+    `
+  },
+  {
+    helpTitle: "Combine the Class Selector",
+    selectorName: "",
+    doThis : "Select the small oranges",
+    selector : "orange.small",
+    syntax: "A.className",
+    help : 'You can combine the class selector with other selectors, like the type selector.',
+    examples : [
+      '<strong>ul.important</strong> selects all <tag>ul</tag> elements that have <strong>class="important"</strong>',
+      '<strong>#big.wide</strong> selects all elements with <strong>id="big"</strong> that also have <strong>class="wide"</strong>'
+    ],
+    boardMarkup :`
+    <apple/>
+    <apple class="small"/>
+    <bento>
+      <orange class="small"/>
+    </bento>
+    <plate>
+      <orange/>
+    </plate>
+    <plate>
+      <orange class="small"/>
+    </plate>`
+  },
+  {
+    helpTitle: "You can do it...",
+    selectorName: "",
+    doThis : "Select the small oranges in the bentos",
+    selector : "bento orange.small",
+    syntax: "Put your back into it!",
+    help : 'Combine what you learned in the last few levels to solve this one!',
+    examples : [],
+    boardMarkup : `
+    <bento>
+      <orange/>
+    </bento>
+    <orange class="small"/>
+    <bento>
+      <orange class="small"/>
+    </bento>
+    <bento>
+      <apple class="small"/>
+    </bento>
+    <bento>
+      <orange class="small"/>
+    </bento>
+    `
   }
 ]
